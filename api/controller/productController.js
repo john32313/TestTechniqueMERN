331081920a5fdb6648a31be69b53f089bd206ed8
john32313 <francois.jonathan13@gmail.com>
2021-02-20 12:18:32 +0100

    add verif one get product exist

--- api/controller/productController.js
+++ api/controller/productController.js
@@ -8,7 +8,11 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   const product = await productModel.get({ _id: Number(req.params.id) });
-  res.json(product);
+  if (product.length === 0)
+    return res
+      .status(404)
+      .json({ message: 'Le produit ne semble pas exister.' });
+  return res.json(product);
 });
 
 module.exports = router;
